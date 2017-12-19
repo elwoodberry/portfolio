e3efc1e8b32cc1c82d50b344b91103a403c6e54c
Elwood Berry <me@elwoodberry.com>
2017-12-19 17:48:40 -0500

    Updated

--- portfolio/prototypes/auth-alpha/README.md
+++ portfolio/prototypes/auth-alpha/README.md
@@ -1,8 +1,27 @@
 # App Access (ALPHA)
 
-Description   | Image   
+## Single Page Access
+This view combines three forms (Sign In, Registration & Forgot Password) into one using CSS's translate and back face visibility.
+
+### Sign In
+As An End User, I have the ability to:
+Learn more about becoming a registered user of this application.
+Sign into an existing account.
+Provide my username associated with my registered account.
+Provide my password associated with my registered account.
+Have my access be cached in my browser so the app "Remembers" me.
+Submit my username and password to access my account.
+Learn more about the terms of use for this application.
+Learn more about the privacy policy for this application.
+Gain help if I have forgotten my username or password.
+Create an account for this application
+Learn more about other legal terms, policies and/or security terms.
+
+Sign In   | Transition    
 :---   | :---:   
-As An End User, I have the ability to:<ul><li>Learn more about becoming a registered user of this application.</li><li>Sign into an existing account.</li><li>Provide my username associated with my registered account.</li><li>Provide my password associated with my registered account.</li><li>Have my access be cached in my browser so the app "Remembers" me.</li><li>Submit my username and password to access my account.</li><li>Learn more about the terms of use for this application.</li><li>Learn more about the privacy policy for this application.</li><li>Gain help if I have forgotten my username or password.</li><li>Create an account for this application</li><li>Learn more about other legal terms, policies and/or security terms.</li></ul>   | ![image](https://raw.github.com/elwoodberry/portfolio/master/_img/app-access-alpha__01.png)   
+![image](https://raw.github.com/elwoodberry/portfolio/master/_img/app-access-alpha__01.png)   | ![image](https://raw.github.com/elwoodberry/portfolio/master/_img/app-access-alpha__02.png)
+
+
 
 ## Inspiration
 1. [Mint](https://www.mint.com/)'s login/sign up UI workflow.
--- portfolio/prototypes/auth-alpha/_project/public/js/main.js
+++ portfolio/prototypes/auth-alpha/_project/public/js/main.js
@@ -1,14 +1,16 @@
 const cardSignIn = document.getElementById('card-signin');
 const cardCreate = document.getElementById('card-create');
 const cardForgot = document.getElementById('card-forgot');
-const linkForgotPassword = document.getElementById('link-forgot-password');
-const linkCreateAccount = document.getElementById('link-create-account');
+const cardSignInClassList = cardSignIn.classList;
+const cardForgotClassList = cardForgot.classList;
+const cardCreateClassList = cardCreate.classList;
+const siginLinkForgotPassword = document.getElementById('link-sigin-forgot-password');
+const siginLinkCreateAccount = document.getElementById('link-sigin-create-account');
+const forgotLinkSignIn = document.getElementById('link-forgot-signin');
+const createLinkSignIn = document.getElementById('create-link-signin');
 
-linkForgotPassword.addEventListener("click", () => {
-  let cardSignInClassList = cardSignIn.classList;
-  let cardForgotClassList = cardForgot.classList;
+siginLinkForgotPassword.addEventListener("click", () => {
   let checkSignIn = cardSignInClassList.contains('hide');
-  let checkForgot = cardForgotClassList.contains('hide');
 
   if(!checkSignIn){
     cardSignInClassList.add('hide');
@@ -16,14 +18,27 @@ linkForgotPassword.addEventListener("click", () => {
   }
 });
 
-linkCreateAccount.addEventListener("click", () => {
-  let cardSignInClassList = cardSignIn.classList;
-  let cardCreateClassList = cardCreate.classList;
+siginLinkCreateAccount.addEventListener("click", () => {
   let checkSignIn = cardSignInClassList.contains('hide');
-  let checkCreate = cardCreateClassList.contains('hide');
 
   if(!checkSignIn){
     cardSignInClassList.add('hide');
     cardCreateClassList.remove('hide');
+  }
+});
+
+forgotLinkSignIn.addEventListener("click", () => {
+  let checkCardForgot = cardForgotClassList.contains('hide');
+  if(!checkCardForgot){
+    cardForgotClassList.add('hide');
+    cardSignInClassList.remove('hide');
+  }
+});
+
+createLinkSignIn.addEventListener("click", () => {
+  let checkCardCreate = cardCreateClassList.contains('hide');
+  if(!checkCardCreate){
+    cardCreateClassList.add('hide');
+    cardSignInClassList.remove('hide');
   }
 });
